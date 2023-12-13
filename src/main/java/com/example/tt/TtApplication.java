fbb5d224456e82d1f8a603eabc0a8d52ff47e757
ChernovDanylo <enge51874@gmail.com>
2023-12-13 22:22:38 +0200

    Final commit

--- src/main/java/com/example/tt/TtApplication.java
+++ src/main/java/com/example/tt/TtApplication.java
@@ -1,39 +1,21 @@
 package com.example.tt;
 
-import com.example.tt.models.dto.responses.RoomResponse;
-import com.example.tt.models.dto.responses.ShortVisitant;
-import com.example.tt.models.entities.Group;
-import com.example.tt.models.entities.Room;
-import com.example.tt.models.entities.Visitant;
-import com.example.tt.models.enums.Type;
-import com.example.tt.repositories.GroupRepo;
-import com.example.tt.repositories.RoomRepo;
-import com.example.tt.repositories.VisitantRepo;
-import com.example.tt.services.PdfGenerator;
-import com.github.javafaker.Faker;
-import org.aspectj.runtime.reflect.Factory;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import javax.xml.crypto.Data;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.*;
-
+/*
+    Стосовно паттернів, я нічого не зміг сюди додати, окрім Білдера. Як на мене, паттерни проєктування
+    створенні для рефакторинга складного коду, щоб зробити його більш ефективним та зрозумілим, але коли
+    структура і код максимально прості в проєкті, я не бачу сенсу навантажувати його лишніми паттернами
+    притягнутими в код за вуха, тому окрім Твірних паттернів таких як Білдер і Сінглтон я нічого більше
+    не використав.
+ */
 @SpringBootApplication
 public class TtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TtApplication.class, args);
-    }
-    @Bean
-    public CommandLineRunner myCommandLineRunner(VisitantRepo visitantRepo) {
-        return args -> {
-        };
     }
 
 }
